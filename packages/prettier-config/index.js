c1d22c0755fb4c9984eed2221f27a4a4d7de76e1
holy1crab <holy1crab@gmail.com>
2024-12-23 14:59:18 +0500

    add `prettier-config`

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: true,
+    bracketSpacing: false,
+    htmlWhitespaceSensitivity: 'ignore',
+    printWidth: 100,
+    semi: false,
+    bracketSameLine: false,
+}
+
+export default config
